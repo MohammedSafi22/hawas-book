3525330ffd30c405e0d45e27f38e498ec54cfcd2
Mohammed_Safi <mohammedsafi335599@gmail.com>
2022-10-20 15:17:04 +0300

    comit2

--- src/Exercises/Exercise1u1/Exercise1.js
+++ src/Exercises/Exercise1u1/Exercise1.js
@@ -1,35 +1,42 @@
-import React from 'react'
-import imge1 from '../../imgs/05.png';
+import React from "react";
+import Image1 from '../../imgs/05.png';
 import "./exercise1.css";
+import StringToReactComponent from 'string-to-react-component';
 
 const Exercise1 = () => {
   return (
-    <div className='container'>
-      <div className='flexy'>
-        <div className='title'>النشاط الأول</div>
-        <div className='name'>إسمي ز ع ت ر</div>
-      </div>
-        <br />
-        <br />
-
-        <div className='text'>ألون حرف الزاي (ز),أول حرف من اسمي واكمل الاحرف الناقصة في المربعات الملائمة حسب الكلمة والصورة</div>
-
-        <div>
-            <img className='image1' src={imge1} alt="" />
+    <StringToReactComponent data={{Image1}}>
+      {`(props)=>{
+         const {Image1}=props;
+         return (<>
+          <div className='container'>
+          <div className='flexy'>
+            <div className='title'>النشاط الأول</div>
+            <div className='name'>إسمي ز ع ت ر</div>
+          </div>
+            <br />
+            <br />
+    
+            <div className='text'>ألون حرف الزاي (ز),أول حرف من اسمي واكمل الاحرف الناقصة في المربعات الملائمة حسب الكلمة والصورة</div>
+    
+            <div>
+                <img className='image1' src={Image1} alt="" />
+            </div>
+            <div className='main'>
+            <div className='side-right'>
+                <div>زملاء</div>
+                <div>ز</div>
+            </div>
+            <div>
+              <img className='image2' src={Image1} alt="" />
+              <div className='complete'>ز     ء</div>
+            </div>
+            </div>
+            
         </div>
-        <div className='main'>
-        <div className='side-right'>
-            <div>زملاء</div>
-            <div>ز</div>
-        </div>
-        <div>
-          <img className='image2' src={imge1} alt="" />
-          <div className='complete'>ز     ء</div>
-        </div>
-        </div>
-        
-    </div>
+         </>);
+       }`}
+    </StringToReactComponent>
   )
 }
-
-export default Exercise1
+export default Exercise1;
